eded88924b3bf4ea6a7d1664faa8aa93a8e4cc6a
samira-estes-garcia <samira.estes.garcia@gmail.com>
2021-02-05 16:25:21 -0600

    build custom om graphql server, blogposts w/ authors

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const schema = require('./schema.js')
+
+const app = express()
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema: schema,
+      graphiql: true,
+    })
+  )
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000')
+})
