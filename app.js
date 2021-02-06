1945b38760c9745f04925fd39b5a281fb5bbc262
samira-estes-garcia <samira.estes.garcia@gmail.com>
2021-02-05 21:29:31 -0600

    changed port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema.js')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
+const cors = require( `cors` )
 
 const app = express()
 
+app.use( cors() );
 app.use(
     '/graphql',
     graphqlHTTP({
